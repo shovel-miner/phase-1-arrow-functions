c251b4e0a16f0531480962dbdecffc85d2cd2594
Hieu Vu Duc Nguyen <vdhieu01@gmail.com>
2022-03-04 15:29:10 -0800

    Completed assignment

---
+++ index.js
@@ -0,0 +1,10 @@
+const divide = (number1,number2) => 2000 / 100 ;  
+const square = (parameter1) => {
+    console.log(parameter1);
+    return parameter1 * parameter1;
+}
+const add = (value1,value2) => {
+    console.log(value1);
+    console.log(value2);
+    return value1 + value2;
+}
